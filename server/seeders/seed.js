c763e094a11967ae8d423ff643c3308384debcfa
nlad218 <nlad218@gmail.com>
2023-10-31 14:38:28 -0600

    added seed data

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,1 +1,5 @@
 const db = require("../config/connection");
+const { dogProfile, eventPage, userProfile } = require("../models");
+const eventSeeds = require("./eventSeeds.json");
+const userSeeds = require("./userSeeds.json");
+const dogSeeds = require("./dogSeeds.json");
