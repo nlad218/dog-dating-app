9e76ba242647eb405dbddd2102416e17621f6a2c
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-10-31 12:44:56 -0600

    have to import daisyui after tailwind typography

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,5 +4,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui"), require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
