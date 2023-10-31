f99fa02cb4537f40c3bef0fd841ba87e178be3d8
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-10-31 11:21:20 -0600

    scaffold front-end file structure

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <main className="card bg-primary text-primary-content">
+      <div className="card-body">
+        <h1 className="card-title">Welcome to the front end &gt;:)</h1>
+        Hopefully this is all working
+      </div>
+    </main>
+  )
+}
+
+export default App
