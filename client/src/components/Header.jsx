d45471a5f185f1a303e8bac9918f6e39400190dc
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-10-31 13:39:23 -0600

    style nav in header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,10 @@
 export default function Header({ children }) {
   return (
-    <header className="flex flex-row justify-center md:justify-between p-4 sticky top-0 min-w-full bg-primary text-primary-content rounded-b-xl">
+    <header className="flex flex-row items-center justify-center md:justify-between p-4 sticky top-0 min-w-full bg-primary text-primary-content rounded-b-xl">
       <h1 className="text-primary-content text-5xl md:text-4xl font-bold">
         Snif
       </h1>
-      <div className="hidden md:inline">{children}</div>
+      <div className="hidden md:inline max-w-xl">{children}</div>
     </header>
   );
 }
--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,6 +1,6 @@
 export default function Nav() {
   return (
-    <nav className="flex flex-row justify-around gap-4 w-full">
+    <nav className="flex flex-row justify-around gap-4 w-full text-2xl font-semibold">
       <div>Home</div>
       <div>Chat</div>
       <div>Profile</div>
