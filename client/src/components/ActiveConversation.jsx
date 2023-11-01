51e5d33210f69f6785b2eb24b082d96e5406101d
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-11-01 11:36:59 -0600

    implement chat bubbles

--- client/src/components/ActiveConversation.jsx
+++ client/src/components/ActiveConversation.jsx
@@ -1,3 +1,19 @@
 export default function ActiveConversation(props) {
+  const messages = [
+    { sender: false, content: "Hello there!" },
+    { sender: true, content: "Hello there!" },
+    { sender: false, content: "Hello there!" },
+    { sender: true, content: "Hello there!" },
+    { sender: false, content: "Hello there!" },
+  ];
 
+  return (
+    <div className="w-full min-h-full">
+      {messages.map((message, index) => (
+      <div className={message.sender ? "chat chat-end" : "chat chat-start"} key={index}>
+        <div className={message.sender ? "chat-bubble chat-bubble-primary" : "chat-bubble"}>{message.content}</div>
+      </div>
+      ))}
+    </div>
+  )
 }
--- client/src/components/ConversationList.jsx
+++ client/src/components/ConversationList.jsx
@@ -1,3 +1,9 @@
 export default function ConversationList(props) {
-
+  return (
+    <>
+      {props.names.map((name, index) => {
+        {(index === props.active) ? <div>active</div> : <div>{name}</div> }
+      })}
+    </>
+  )
 }
