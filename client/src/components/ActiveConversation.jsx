f2a716b9aeec396ada45ad65cb84158c47ca19b1
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-11-01 10:37:59 -0600

    skeleton layout for chat page

---
+++ client/src/components/ActiveConversation.jsx
@@ -0,0 +1,3 @@
+export default function ActiveConversation(props) {
+
+}
--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,3 +1,29 @@
+import { useState } from "react"
+import ConversationList from "../components/ConversationList";
+import ActiveConversation from "../components/ActiveConversation";
+
 export default function Chat() {
-  return <div>This is the chat page.</div>;
+  // This page will need to:
+  // - Query the server for the user's connections
+  // - Query the server for some amount of messages for EVERY chat (at least the single most recent & when it was sent)
+  // - Query the server for and/or cache some amount of messages for the active conversation
+  // - Keep track of the active conversation
+  // - Possibly open a websocket for the current conversation?
+
+  const [activeConversation, setActiveConversation] = useState(0)
+
+  const tempNameList = ["Nick F", "Nick D", "Jake", "Maya", "Josh"]
+
+  return (
+    <div className="bg-base-100 shadow-xl rounded-xl max-w-5xl">
+      <div>
+        THIS WILL APPEAR ABOVE THE LIST OF CONVERSATIONS
+        <ConversationList names={tempNameList} activeConversation={activeConversation} setActiveConversation={setActiveConversation} />
+      </div>
+      <div>
+        THIS WILL APPEAR ABOVE THE ACTIVE CONVERSATION
+        <ActiveConversation />
+      </div>
+    </div>
+  )
 }
