5d2d53cee6cea81b59be5504c5ecf585225ea68c
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-11-02 11:28:52 -0600

    write apollo client queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -29,11 +29,56 @@ export const QUERY_SELF_MATCHES = gql`
 `;
 
 export const QUERY_MATCH_MESSAGES = gql`
-  query matchMessages {
-    
+  query matchMessages($matchId: ID!) {
+    oneMatch(matchId: $matchID) {
+      user1
+      user2
+      messages {
+        user
+        messageText
+      }
+    }
   }
 `;
 
-export const QUERY_LIKES = gql``;
+export const QUERY_USER_LIKES = gql`
+  query userLikes($userId: ID!) {
+    getLikes(userId: $userId) {
 
-export const QUERY_RANDOM_USERS = gql``;
+    }
+  }
+`;
+
+export const QUERY_DISPLAYABLE_USERS = gql`
+  query showableUsers {
+    getRandomUsers {
+      _id
+      ownerName
+      dogName
+      image
+      breed
+      age
+      size
+      about
+      hobbies
+      likes
+    }
+  }
+`;
+
+export const QUERY_FILTER_BY_BREED = gql`
+  query filteredUsers {
+    filterUsersByBreed {
+      _id
+      ownerName
+      dogName
+      image
+      breed
+      age
+      size
+      about
+      hobbies
+      likes
+    }
+  }
+`;
