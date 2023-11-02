a73699f85aa32c1738f2e9b1b1e65527ef5deaab
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-11-02 15:21:38 -0600

    base route redirects to main or landing page depending on if the user is logged in or not

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,43 +5,40 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App.jsx";
 import LandingPage from "./pages/Home";
-import Main from "./pages/Main";
+import MainPage from "./pages/MainPage.jsx";
 import Chat from "./pages/Chat";
 import Profile from "./pages/Profile";
-import ExImgUpload from "./pages/ExImgUpload.jsx"
+import ExImgUpload from "./pages/ExImgUpload.jsx";
+
+import Auth from "./utils/auth.js";
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <App />,
-		children: [
-			{
-				// ALL non-users should be redirected to this page, no matter what
-				index: true,
-				element: <LandingPage />,
-			},
-			{
-				path: "/main",
-				element: <Main />,
-			},
-			{
-				path: "/chat",
-				element: <Chat />,
-			},
-			{
-				path: "/profile",
-				element: <Profile />,
-			},
-			{
-				path: '/imgupload',
-				element: <ExImgUpload />
-			}
-		],
-	},
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: Auth.loggedIn() ? <MainPage /> : <LandingPage />,
+      },
+      {
+        path: "/chat",
+        element: <Chat />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+      {
+        path: "/imgupload",
+        element: <ExImgUpload />,
+      },
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<RouterProvider router={router} />
-	</React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
