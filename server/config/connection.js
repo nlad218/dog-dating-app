0f12e9e628024102d01c519eb0c310fd6c5f35e3
nlad218 <nlad218@gmail.com>
2023-11-04 14:21:31 -0600

    reverted connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,19 +1,5 @@
-const Mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/snif');
-
-const mongoose = pprocess.env.JAWSDB_URL
-? new Mongoose(process.env.JAWSDB_URL)
-: new Mongoose(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: "127.0.0.1",
-      dialect: "mysql",
-      port: 3306,
-    }
-  );
-
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/snif");
 
 module.exports = mongoose.connection;
