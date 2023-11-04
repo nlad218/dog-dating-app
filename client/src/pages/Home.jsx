0991e5db52b84ea29a9bb9a31974fe5b6f43e470
nfillip <nick.m.fillip@gmail.com>
2023-11-04 10:39:57 -0600

    adjusted hero-overlay on main page for background image style to be less orange

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -69,7 +69,7 @@ export default function LandingPage() {
 						"url(https://img.freepik.com/premium-photo/group-dogs-are-sitting-together-park_902338-22817.jpg)",
 				}}
 			>
-				<div className="hero-overlay bg-opacity-60"></div>
+				<div className="hero-overlay bg-opacity-20"></div>
 				<div className="hero-content text-center text-neutral-content">
 					<div className="max-w-md">
 						<h1 className="mb-5 text-5xl font-bold">
