8dbe2c51449118f3dd32675f787f12818a104104
nfillip <nick.m.fillip@gmail.com>
2023-11-05 16:10:25 -0700

    serarching for server bug

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -18,7 +18,6 @@ export default function Profile() {
 		}
 	} );
 	const [update] = useMutation(UPDATE_USER);
-	const [deleteUser] = useMutation(DELETE_USER);
 	const [userData1, setNewUserData1] = useState({})
 	const userData = data?.me || {};
 	const loggedIn = Auth.loggedIn();
